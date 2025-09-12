f4a637786f0b9bd2d01992c6c8668ca0c03c3296
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-09-12 23:22:49 +0530

    Shipping Layer

---
+++ shippingProcessing/src/main/java/com/event/shippingProcessing/model/Shipping.java
@@ -0,0 +1,36 @@
+package com.event.shippingProcessing.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter
+@ToString
+public class Shipping {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "shippingId", nullable = false)
+    private Integer shippingId;
+
+    @Column(name="address", nullable = false)
+    private String address;
+
+    @Column(name="orderStatus", nullable = false)
+    private String  orderStatus;
+
+    public Shipping() {
+
+    }
+
+    public Shipping(String address, String orderStatus) {
+        this.address = address;
+        this.orderStatus = orderStatus;
+    }
+}
---
+++ shippingProcessing/src/main/java/com/event/shippingProcessing/repository/ShippingDAO.java
@@ -0,0 +1,14 @@
+package com.event.shippingProcessing.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.event.shippingProcessing.model.Shipping;
+
+public interface ShippingDAO extends JpaRepository<Shipping, Integer>{
+    
+    @SuppressWarnings("null")
+    List<Shipping> findAll();
+
+    Shipping findById(int shippingId);
+}
