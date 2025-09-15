06ef8f0ce1c5f93f08411062b1176a811ae7edfc
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-09-15 17:11:04 +0530

    User defined Exception

---
+++ inventoryProcessing/src/main/java/com/event/inventoryProcessing/Exceptions/ItemNotFoundException.java
@@ -0,0 +1,7 @@
+package com.event.inventoryProcessing.Exceptions;
+
+public class ItemNotFoundException extends Exception {
+    public ItemNotFoundException(String message) {
+        super(message);
+    }
+}
