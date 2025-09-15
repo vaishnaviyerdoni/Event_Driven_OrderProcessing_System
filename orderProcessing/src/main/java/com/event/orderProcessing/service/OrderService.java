7fce70fc0d73e7618a4375a3ff45295e83dde987
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-09-15 22:43:40 +0530

    Service layer

---
+++ orderProcessing/src/main/java/com/event/orderProcessing/service/OrderService.java
@@ -0,0 +1,132 @@
+package com.event.orderProcessing.service;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.event.orderProcessing.DTO.AddOrder;
+import com.event.orderProcessing.Exception.OrderNotFoundException;
+import com.event.orderProcessing.model.Order;
+import com.event.orderProcessing.repository.OrderDAO;
+
+@Service
+public class OrderService {
+    
+    private OrderDAO orderDAO;
+
+    public OrderService(OrderDAO orderDAO) {
+        this.orderDAO = orderDAO;
+    }
+
+    //POST method : To place an Order
+    public int placeOrder(AddOrder order) {
+        try{
+            LocalDate orderDate = order.getOrderDate();
+            String orderStatus = order.getOrderStatus();
+            Double totalAmount = order.getTotalAmount();
+
+            Order myOrder = new Order(orderDate, orderStatus, totalAmount);
+            Order newOrder = orderDAO.save(myOrder);
+            return newOrder.getOrderId();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    //GET method : To get all the Orders
+    public List<Order> getAllOrders() {
+        try{
+            List<Order> orders = orderDAO.findAll();
+            if(orders != null) {
+                return orders;
+            }
+            else{
+                throw new OrderNotFoundException("Orders are absent");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    //GET method : To get Order by its Id
+    public Order getById(int orderId) {
+        try{
+            Order order = orderDAO.findById(orderId);
+            if(order != null) {
+                return order;
+            }
+            else{
+                throw new OrderNotFoundException("Order not found!");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return new Order();
+        }
+    }
+
+    //GET method : To get Orders by status
+    public List<Order> getOrderbyStatus(String status){
+        try{
+            List<Order> orders = orderDAO.findByOrderStatus(status);
+            if(orders != null) {
+                return orders;
+            }
+            else{
+                throw new OrderNotFoundException("Order for this status not found!");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+    //GET method : To get Price of order
+    public Double getPrice(int orderId) {
+        Double price = 0.0d;
+        try{
+            price = orderDAO.findPriceByOrderId(orderId);
+            if(price != null) {
+                return price;
+            }
+            else{
+                throw new OrderNotFoundException("Price for this Order is absent");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return price;
+        }
+    }
+
+    //UPDATE method : To Update the orderStatus
+    public boolean updateStatus(int orderId, String newOrderStatus) {
+        try{
+            Order order = orderDAO.findById(orderId);
+            order.setOrderStatus(newOrderStatus);
+            orderDAO.save(order);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+
+    //DELETE method : To Delete an Order
+    public boolean deleteOrder(int orderId) {
+        try{
+            Order order = orderDAO.findById(orderId);
+            orderDAO.delete(order);
+            return true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
