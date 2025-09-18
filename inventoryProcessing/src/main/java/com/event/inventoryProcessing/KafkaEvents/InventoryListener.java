3ec4173fa2c8cc9cf1435312f2ea4ca5ca9ca35d
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-09-18 23:33:08 +0530

    Kafka Events

--- inventoryProcessing/src/main/java/com/event/inventoryProcessing/KafkaEvents/InventoryListener.java
+++ inventoryProcessing/src/main/java/com/event/inventoryProcessing/KafkaEvents/InventoryListener.java
@@ -1,34 +1,53 @@
 package com.event.inventoryProcessing.KafkaEvents;
 
+import java.util.List;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import com.event.shared_events.dto.OrderItem;
+import com.event.inventoryProcessing.service.InventoryService;
+import com.event.shared_events.dto.*;
 
 @Component
-public class InventoryListener {
+public class InventoryListener { // Listens from OrderPublisher
 
     private final InventoryPublisher publisher;
+    private final InventoryService service;
 
-    public InventoryListener(InventoryPublisher publisher) {
+    public InventoryListener(InventoryPublisher publisher, InventoryService service) {
         this.publisher = publisher;
+        this.service = service;
     }
     
     //this method listens message from Order, verifies the inventory and call Publisher
     //Passes Event String to publisher
     @KafkaListener(topics = "${general.order.kafka-topic}", groupId = "inventoryProcessing")
 
-    public void consumeOrder(String message) {
+    public void consumeOrder(String message, OrderPlaced order) {
 
         System.out.println("Event received from OrderPublisher: " + message);
 
         if(message.equals("OrderPlaced")) {
             
-            OrderItem item = new OrderItem();
-            item.getItemId();
-            publisher.sendInventoryMessage("InventoryReserved");
-        }
-        else{
-            publisher.sendInventoryMessage("InventoryFailed");
+           List<OrderItem> items = order.getItems();
+           boolean isAvailable = items.stream().allMatch(item -> service.isAvailable(item.getItemId()));
+           
+            InventoryEvent event = new InventoryEvent();
+
+            if(isAvailable){
+                event.setOrderId(order.getOrderId());
+                event.setInventoryStatus("InventoryReserved");
+                event.setOrderStatus("Pending");
+                event.setItems(items);
+
+                publisher.sendInventoryMessage("InventoryReserved", event);
+            }
+            else{
+                event.setOrderId(order.getOrderId());
+                event.setInventoryStatus("InventoryFailed");
+                event.setOrderStatus("Pending");
+                event.setItems(items);
+
+                publisher.sendInventoryMessage("InventoryFailed", event);
+            }
         }
     }
 }
--- inventoryProcessing/src/main/java/com/event/inventoryProcessing/KafkaEvents/InventoryPublisher.java
+++ inventoryProcessing/src/main/java/com/event/inventoryProcessing/KafkaEvents/InventoryPublisher.java
@@ -1,21 +1,22 @@
 package com.event.inventoryProcessing.KafkaEvents;
 
+import com.event.shared_events.dto.*;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class InventoryPublisher {
+public class InventoryPublisher { //Publishes event to PaymentListener
     
-    private final KafkaTemplate<String, String> kafkaTemp;
+    private final KafkaTemplate<String, Object> kafkaTemp;
 
-    public InventoryPublisher(KafkaTemplate<String, String> kafkaTemp) {
+    public InventoryPublisher(KafkaTemplate<String, Object> kafkaTemp) {
         this.kafkaTemp = kafkaTemp;
     }
 
-    public void sendInventoryMessage(String message) {
+    public void sendInventoryMessage(String message, InventoryEvent event) {
         try{
-            kafkaTemp.send("Message", message);
-            System.out.println("Message Received From Listener: " + message);
+            kafkaTemp.send(message, event);
+            System.out.println("Message Received From Listener: " + message); //message == InventoryReserved OR InventoryFailed
         }
         catch(Exception e) {
             e.printStackTrace();
