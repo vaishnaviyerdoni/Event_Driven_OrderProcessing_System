a46252d4d5cf758ce18c2a08f759f41e7f9e34c0
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-09-19 17:06:36 +0530

    Exception

---
+++ paymentProcessing/src/main/java/com/event/paymentProcessing/Exception/PaymentException.java
@@ -0,0 +1,7 @@
+package com.event.paymentProcessing.Exception;
+
+public class PaymentException extends Exception {
+    public PaymentException(String message) {
+        super(message);
+    }
+}
---
+++ shippingProcessing/src/main/java/com/event/shippingProcessing/Exception/ShipmentException.java
@@ -0,0 +1,7 @@
+package com.event.shippingProcessing.Exception;
+
+public class ShipmentException extends Exception {
+    public ShipmentException(String message) {
+        super(message);
+    }    
+}
