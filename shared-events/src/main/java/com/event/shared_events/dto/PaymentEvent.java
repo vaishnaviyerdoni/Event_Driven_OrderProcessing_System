fe3e93348f6a77608b885eb2090921a35f93f9d7
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-09-24 19:42:47 +0530

    shared dto

---
+++ shared-events/src/main/java/com/event/shared_events/dto/PaymentEvent.java
@@ -0,0 +1,18 @@
+package com.event.shared_events.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentEvent {
+    private Integer orderId;
+    private List<OrderItem> items;
+    private Double amount;
+    private String orderStatus;
+}
