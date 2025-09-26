008c24d22a5c954a25ed72261e1ed3a23fb0c33b
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-09-26 17:57:07 +0530

    Payment Kafka Events

--- paymentProcessing/src/main/java/com/event/paymentProcessing/KafkaEvents/PaymentListener.java
+++ paymentProcessing/src/main/java/com/event/paymentProcessing/KafkaEvents/PaymentListener.java
@@ -2,19 +2,41 @@ package com.event.paymentProcessing.KafkaEvents;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import com.event.paymentProcessing.DTO.MakePayment;
 import com.event.paymentProcessing.service.PaymentService;
 import com.event.shared_events.dto.*;
 
 @Component
 public class PaymentListener {
     private final PaymentService service;
+    private final PaymentPublisher publisher;
 
-    public PaymentListener(PaymentService service) {
+    public PaymentListener(PaymentService service, PaymentPublisher publisher) {
         this.service = service;
+        this.publisher = publisher;
     }
 
     @KafkaListener(topics = "${general.inventory.kafka-topic}", groupId = "paymentProcessing")
-    public void consumeInventory(String message, PaymentEvent event){
+    public void consumeInventory(InventoryEvent event){
+
+        MakePayment payment =  new MakePayment(event.getTotalAmount(), event.getOrderStatus());
+        PaymentEvent payEvent = new PaymentEvent();
+
+        payEvent.setAmount(event.getTotalAmount());
+        payEvent.setOrderId(event.getOrderId());
+        payEvent.setItems(event.getItems());
+        payEvent.setOrderStatus(event.getOrderStatus());
         
+        if("InventoryReserved".equals(event.getInventoryStatus())){
+            service.payOrder(payment);
+            payEvent.setPaymentStatus("PaymentSuccessful");
+
+            publisher.sendPaymentMessage(payEvent);
+        }
+        else{
+            payEvent.setPaymentStatus("PaymentFailed");
+            publisher.sendPaymentMessage(payEvent);
+        }
     }
 }
--- paymentProcessing/src/main/java/com/event/paymentProcessing/KafkaEvents/PaymentPublisher.java
+++ paymentProcessing/src/main/java/com/event/paymentProcessing/KafkaEvents/PaymentPublisher.java
@@ -1,5 +1,18 @@
 package com.event.paymentProcessing.KafkaEvents;
 
+import org.springframework.kafka.core.KafkaTemplate;
+import com.event.shared_events.dto.*;
+
 public class PaymentPublisher {
     
+    private KafkaTemplate<String, Object> kafkaTemp;
+
+    public PaymentPublisher(KafkaTemplate<String, Object> kafkaTemp) {
+        this.kafkaTemp = kafkaTemp;
+    }
+
+    public void sendPaymentMessage(PaymentEvent event){
+        kafkaTemp.send(event.getPaymentStatus(), event);
+        System.out.println("Message Received From Listener: " + event.getPaymentStatus());
+    }
 }
