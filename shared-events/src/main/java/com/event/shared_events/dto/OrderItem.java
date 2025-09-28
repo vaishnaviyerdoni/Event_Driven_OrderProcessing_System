bfebd2a980e0663c60c8503e7e00afb60e886391
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-09-28 23:07:59 +0530

    shared dto minor changes

--- shared-events/src/main/java/com/event/shared_events/dto/OrderItem.java
+++ shared-events/src/main/java/com/event/shared_events/dto/OrderItem.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class OrderItem {
+    private Integer orderId;
     private Integer itemId;
     private String itemname;
     private int quantity;
@@ -14,7 +15,8 @@ public class OrderItem {
 
     }
 
-    public OrderItem (Integer itemId, String itemname, int quantity) {
+    public OrderItem (Integer orderId, Integer itemId, String itemname, int quantity) {
+        this.orderId = orderId;
         this.itemId = itemId;
         this.itemname = itemname;
         this.quantity = quantity;
