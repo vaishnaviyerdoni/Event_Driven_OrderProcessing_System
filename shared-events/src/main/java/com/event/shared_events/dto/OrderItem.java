4678031267c65d641df2b6ef74868684a7b70861
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-09-29 17:06:20 +0530

    shared dto

--- shared-events/src/main/java/com/event/shared_events/dto/OrderItem.java
+++ shared-events/src/main/java/com/event/shared_events/dto/OrderItem.java
@@ -8,17 +8,15 @@ import lombok.Setter;
 public class OrderItem {
     private Integer orderId;
     private Integer itemId;
-    private String itemname;
     private int quantity;
 
     public OrderItem() {
 
     }
 
-    public OrderItem (Integer orderId, Integer itemId, String itemname, int quantity) {
+    public OrderItem (Integer orderId, Integer itemId, int quantity){
         this.orderId = orderId;
         this.itemId = itemId;
-        this.itemname = itemname;
         this.quantity = quantity;
     }
 }
--- shared-events/src/main/java/com/event/shared_events/dto/OrderPlaced.java
+++ shared-events/src/main/java/com/event/shared_events/dto/OrderPlaced.java
@@ -1,27 +1,19 @@
 package com.event.shared_events.dto;
 
 import java.util.List;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor @NoArgsConstructor
 public class OrderPlaced {
     private Integer orderId;
     private List<OrderItem> items;
     private String orderStatus;
     private Double totalAmount;
     private String address;
-
-    public OrderPlaced() {
-
-    }
-
-    public OrderPlaced(Integer orderId, List<OrderItem> items, String orderStatus, Double totalAmount, String address) {
-        this.orderId = orderId;
-        this.items = items;
-        this.orderStatus = orderStatus;
-        this.totalAmount = totalAmount;
-        this.address = address;
-    }
 }
