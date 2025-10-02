d9e8854b0331e3318da97fcd4b946bc903d3f2c8
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-10-02 17:46:49 +0530

    Exception handler

---
+++ orderProcessing/src/main/java/com/event/orderProcessing/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.event.orderProcessing.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.event.shared_events.dto.ErrorResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(OrderNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+              .body(new ErrorResponse(404, e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralError(Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+               .body(new ErrorResponse(500, "Unexpected Error Occured"));
+    }
+}
+
