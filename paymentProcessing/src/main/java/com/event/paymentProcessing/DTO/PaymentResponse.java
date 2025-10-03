76ff4d2dbd12ca2063d9a870cd41732e71f7a723
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-10-03 16:40:32 +0530

    dto

---
+++ paymentProcessing/src/main/java/com/event/paymentProcessing/DTO/PaymentResponse.java
@@ -0,0 +1,15 @@
+package com.event.paymentProcessing.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentResponse {
+    private Integer paymentId;
+    private String message;
+}
